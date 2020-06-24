b731cb15b7e8c21de0d9e102355edbdfe84b0660
linux-jedi <cbthomas1997@gmail.com>
2020-06-24 13:16:08 -0500

    Add helper functions for memoization pass
    Add helper functions defined in compile time function pass paper.Functions
    are partially defined and some are missing extra helpers.

--- llvm/lib/Transforms/Memoize/Memoize.cpp
+++ llvm/lib/Transforms/Memoize/Memoize.cpp
@@ -18,6 +18,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Pass.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -32,8 +33,30 @@ namespace {
     bool runOnModule(Module &M) override {
       errs() << "Memoize: ";
       errs().write_escaped(M.getName()) << '\n';
+
+      // Do work
+      for (Function &F: M.getFunctionList()) {
+        if (isMemoizable(F)) {
+          // make function memoizable
+
+          // update all call sites 
+
+          // send meta data to if-memo
+        }
+      }
       return false;
     }
+
+    private:
+    bool isGlobalSafe(Function &F);
+    bool isMemoizable(Function &F);
+    bool isMemoizableLib(Function &F);
+    bool isMemoizablePointer(Argument &A);
+    bool isPointerType(Argument &A);
+    bool isProperArguments(Function &F);
+    bool checkFunctionCalls(Function &F);
+    bool mayBeOverridden(Function &F);
+
   };
 }
 
@@ -41,3 +64,46 @@ char Memoize::ID = 0;
 static RegisterPass<Memoize> X("memoize", "Function Memoize Pass",
                              false /* Call site replacement modifies CFG */,
                              false /* This is not an analysis pass */);
+
+bool Memoize::isGlobalSafe(Function &F) {
+  // check if function uses global variable
+}
+
+bool Memoize::isMemoizable(Function &F) {
+  if (F.isDeclaration() 
+    || F.isIntrinsic() || F.isVarArg() || mayBeOverridden(F))
+    return false;
+
+  if (isMemoizableLib(F))
+    return true;
+
+  size_t count = 0;
+  if (isProperArguments(F) && isGlobalSafe(F) && checkFunctionCalls(F))
+    return true;
+
+  return false;
+}
+
+bool Memoize::isMemoizablePointer(Argument &A) {
+  // check each use of A
+  //   if any are load or store then not memoizable
+  for (User *U: A.users()) {
+    if (!isa<LoadInst>(U) && !isa<StoreInst>(U))
+      return false;
+  }
+  return true; // TODO: reread paper to determine if this is true
+}
+
+bool Memoize::isProperArguments(Function &F) {
+  for (Argument &A: F.args()) {
+    if (isPointerType(A)) {
+      if (isMemoizablePointer(A))
+        continue;
+      return false;
+    }
+  }
+}
+
+bool Memoize::checkFunctionCalls(Function &F) {
+  // Check to see if function is safe for memoization
+}
