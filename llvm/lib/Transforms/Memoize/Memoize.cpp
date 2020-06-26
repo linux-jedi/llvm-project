ca9865071c5878544ed13a6d5184f586de34c65a
linux-jedi <cbthomas1997@gmail.com>
2020-06-26 13:27:45 -0500

    Implement isGlobalSafe and checkFunctionsCalls
    Implement two more functions defined in paper. replaceCallSite and
    functions not defined in paper still need to be implemented.

--- llvm/lib/Transforms/Memoize/Memoize.cpp
+++ llvm/lib/Transforms/Memoize/Memoize.cpp
@@ -25,6 +25,14 @@
 using namespace llvm;
 
 namespace {
+  // TODO: Lookup how to document constants with llvm/doxygen
+  // When determining if a function is eligible for memoization, all
+  // function calls within a function are recursively checked for 
+  // side effects and memoization eligibility. MAX_DEPTH of 10 is arbitrarily
+  // chosen by the authors of the paper as the maximum depth the transofrmation
+  // will travel. 
+  size_t MAX_DEPTH = 10;
+
   class Memoize : public ModulePass {
     public:
     static char ID;
@@ -33,6 +41,9 @@ namespace {
     bool runOnModule(Module &M) override {
       errs() << "Memoize: ";
       errs().write_escaped(M.getName()) << '\n';
+
+      // Setup
+      callStackDepth = 0;
 
       // Do work
       for (Function &F: M.getFunctionList()) {
@@ -48,14 +59,17 @@ namespace {
     }
 
     private:
-    bool isGlobalSafe(Function &F);
-    bool isMemoizable(Function &F);
-    bool isMemoizableLib(Function &F);
-    bool isMemoizablePointer(Argument &A);
-    bool isPointerType(Argument &A);
-    bool isProperArguments(Function &F);
-    bool checkFunctionCalls(Function &F);
-    bool mayBeOverridden(Function &F);
+    bool isGlobalSafe(const Function &F);
+    bool isMemoizable(const Function &F);
+    bool isMemoizableLib(const Function &F);
+    bool isMemoizablePointer(const Argument &A);
+    bool isProperArguments(const Function &F);
+    bool checkFunctionCalls(const Function &F);
+    bool mayBeOverridden(const Function &F);
+
+    void replaceCallSites(const Function &F);
+
+    size_t callStackDepth;
 
   };
 }
@@ -65,45 +79,103 @@ static RegisterPass<Memoize> X("memoize", "Function Memoize Pass",
                              false /* Call site replacement modifies CFG */,
                              false /* This is not an analysis pass */);
 
-bool Memoize::isGlobalSafe(Function &F) {
-  // check if function uses global variable
+// Check function's use of global variables 
+bool Memoize::isGlobalSafe(const Function &F) {
+  callStackDepth = 0;
+  const GlobalVariable *global = nullptr;
+
+  for (const BasicBlock &BB: F.getBasicBlockList()) {
+    for (const Instruction &I: BB.instructionsWithoutDebug()) {
+      for (const Use &O: I.operands()) {
+        auto *globalVariable = dyn_cast<GlobalVariable>(&O);
+        if (!globalVariable) continue;
+
+        // If a function uses a global that is not a basic type
+        // (float, double, int) do not memoize it
+        PointerType *pointerType = globalVariable->getType();
+        Type::TypeID opType = pointerType->getTypeID();
+        if (opType != Type::IntegerTyID || 
+            opType != Type::FloatTyID || opType != Type::DoubleTyID)
+            return false;
+
+        // If more than one global variable is in use, the function 
+        // should not be memoized.
+        if (!global) {
+          global = globalVariable;
+          continue;
+        }
+        if (globalVariable == global) continue;
+
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-bool Memoize::isMemoizable(Function &F) {
-  if (F.isDeclaration() 
-    || F.isIntrinsic() || F.isVarArg() || mayBeOverridden(F))
+bool Memoize::isMemoizable(const Function &F) {
+  if (F.isDeclaration() ||
+      F.isIntrinsic() || F.isVarArg() || mayBeOverridden(F))
     return false;
 
   if (isMemoizableLib(F))
     return true;
 
-  size_t count = 0;
+  callStackDepth = 0;
   if (isProperArguments(F) && isGlobalSafe(F) && checkFunctionCalls(F))
     return true;
 
   return false;
 }
 
-bool Memoize::isMemoizablePointer(Argument &A) {
+bool Memoize::isMemoizablePointer(const Argument &A) {
   // check each use of A
   //   if any are load or store then not memoizable
-  for (User *U: A.users()) {
+  for (const User *U: A.users()) {
     if (!isa<LoadInst>(U) && !isa<StoreInst>(U))
       return false;
   }
   return true; // TODO: reread paper to determine if this is true
 }
 
-bool Memoize::isProperArguments(Function &F) {
-  for (Argument &A: F.args()) {
-    if (isPointerType(A)) {
+bool Memoize::isProperArguments(const Function &F) {
+  for (const Argument &A: F.args()) {
+    if (A.getType()->isPointerTy()) {
       if (isMemoizablePointer(A))
         continue;
       return false;
     }
   }
+  return true;
 }
 
-bool Memoize::checkFunctionCalls(Function &F) {
+bool Memoize::checkFunctionCalls(const Function &F) {
   // Check to see if function is safe for memoization
+  for (const BasicBlock &BB: F.getBasicBlockList()) {
+    for (const Instruction &I: BB.instructionsWithoutDebug()) {
+      auto CI = dyn_cast<CallInst>(&I);
+      if (!CI) continue;
+
+      Function* calledFunction = CI->getCalledFunction();
+
+      // TODO: figure out how to add `calledFunction == I`
+      if (calledFunction->isSpeculatable()) { 
+        errs() << "Pure Function: ";
+        errs().write_escaped(calledFunction->getName()) << '\n';
+        continue;
+      }
+      
+      callStackDepth++;
+      if (callStackDepth >= MAX_DEPTH) return false;
+
+      if (isMemoizable(*calledFunction)) continue;
+
+      return false;
+    }
+  }
+  return true;
+}
+
+void replaceCallSites(const Function &F) {
+  
 }
